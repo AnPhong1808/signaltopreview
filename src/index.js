094331ec536e5bc34269423e451c6dbcbd47b40e
anphong1808 <anphong.viettech@gmail.com>
2025-04-07 10:13:06 +0700

    Fix initial route issue by updating redirect in index.js with replaceState

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import AppWrapper from './App';
 
 // Kiểm tra và redirect nếu không có hash
 if (!window.location.hash || window.location.hash === '#') {
-  window.location.hash = '#/';
+  // Thay đổi URL để bao gồm '#/'. Sử dụng replaceState để không tạo thêm lịch sử
+  window.history.replaceState(null, null, window.location.pathname + window.location.search + '#/');
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
